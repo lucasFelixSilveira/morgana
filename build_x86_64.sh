5ecaba5f44b9fe84265e563d4d5aeec8767933f4
LucasFelixSilveira <lucasdwbfff@gmail.com>
2026-05-22 11:18:51 -0300

    chore(eva): updating eva as a dynamic library

--- build_x86_64.sh
+++ build_x86_64.sh
@@ -2,8 +2,13 @@
 
 cd compiler
 
-g++ -std=c++17 -g -O0 -fPIC -fpermissive -fexceptions -llzma -lm main.cpp libs/linux/x86_64/libruna.a libs/linux/x86_64/libeva.a \
+g++ -std=c++17 -g -O0 -fPIC -fpermissive -fexceptions -llzma -lm main.cpp libs/linux/x86_64/libruna.a \
+    -L./libs/linux/x86_64/ -leva \
     -o ../bin/morgana \
     -I. \
-    -lstdc++
+    -lstdc++ \
+    -Wl,-rpath,'$ORIGIN'
+
+cp libs/linux/x86_64/libeva.so ../bin/
+
 cd ..
