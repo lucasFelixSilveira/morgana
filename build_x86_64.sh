441a37039fc7950700de1cba793d096de1eb5a56
LucasFelixSilveira <lucasdwbfff@gmail.com>
2026-05-26 11:44:06 -0300

    chore(scripts): adding runa as a dynamic library and adding to copy runa lib to bin path

--- build_x86_64.sh
+++ build_x86_64.sh
@@ -2,13 +2,14 @@
 
 cd compiler
 
-g++ -std=c++17 -g -O0 -fPIC -fpermissive -fexceptions -llzma -lm main.cpp libs/linux/x86_64/libruna.a \
-    -L./libs/linux/x86_64/ -leva \
+g++ -std=c++17 -g -O0 -fPIC -fpermissive -fexceptions -llzma -lm main.cpp \
+    -L./libs/x86_64-linux -leva -lruna \
     -o ../bin/morgana \
     -I. \
     -lstdc++ \
     -Wl,-rpath,'$ORIGIN'
 
 cp libs/linux/x86_64/libeva.so ../bin/
+cp libs/linux/x86_64/libruna.so ../bin/
 
 cd ..
