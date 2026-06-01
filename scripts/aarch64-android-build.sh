a7a79e8ef774c0654626eb7192017b5b81ef8f1a
LucasFelixSilveira <lucasdwbfff@gmail.com>
2026-06-01 19:42:01 -0300

    chore(scripts): updating the android build script

--- scripts/aarch64-android-build.sh
+++ scripts/aarch64-android-build.sh
@@ -2,10 +2,11 @@
 
 cd compiler
 
-g++ -std=c++17 -g -O0 -fPIC -fpermissive -fexceptions main.cpp \
-    -llua \
-    -lm \
-    -ldl \
-    -o morgana
+clang++ -std=c++17 -g -O0 -fPIC -fpermissive -fexceptions -llzma -lm main.cpp \
+    -L./libs/aarch64-android -leva -lruna \
+    -o ../bin/morgana \
+    -I. \
+    -lstdc++ \
+    -Wl,-rpath,'$ORIGIN'
 
 cd ..
